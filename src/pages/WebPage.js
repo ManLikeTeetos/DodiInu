996f8339921b4b7e358fb09be2b59c86ffd6dcf9
Ade-ojo Toluwanimi <59847687+ManLikeTeetos@users.noreply.github.com>
2024-04-08 11:36:18 +0000

    About us Section

--- src/pages/WebPage.js
+++ src/pages/WebPage.js
@@ -1,5 +1,6 @@
 import "../styles/WebPage.css";
 import "../styles/Hero.css";
+import "../styles/About.css";
 import DodiLogo from '../styles/assets/images/Dodilogo.svg';
 
 const colorStyle = {
@@ -51,61 +52,151 @@ function WebPage(){
 			  </div>
 		  </div>
 	  </div>
-		  <div className="hero">
-			<div className="hero-bg">
-			  <div className="hero-2">
-				  <div className="hero-3">
-					  <div className="column-hero">
-						  <img
-							  loading="lazy"
-							  srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&"
-							  className="img-hero"
-						  />
-					  </div>
-					  <div className="column-hero-2">
-						  <div className="hero-4">
-							  <div className="hero-5">We all gonna make it</div>
-							  <div className="hero-6">
-								  Lorem ipsum dolor sit amet consectetur. Nullam in diam cursus
-								  purus vitae lectus a. Egestas arcu convallis morbi leo quis
-								  velit. Dis faucibus{" "}
-							  </div>
-							  <div className="hero-7">
-								  <div className="hero-8">
-									  <div className="column-hero-3">
-										  <div className="hero-9">BUY NOW</div>
-									  </div>
-									  <div className="column-hero-4">
-										  <div className="hero-10">STAKE NOW</div>
-									  </div>
-								  </div>
-							  </div>
-							  <div className="hero-11">
-								  CA: D6NewxS3SPkeEEbvGQax2E4MiCjuvjAhTUkboXZLwKM1
-							  </div>
-						  </div>
-					  </div>
-				  </div>
-			  </div>
-			  <div className="hero-12">
-				  <div className="hero-13">
-					  <div className="hero-14">$DODI Price </div>
-					  <div className="hero-15">$0.00000412</div>
-				  </div>
-				  <div className="hero-16">
-					  <div className="hero-17">TOTAL DODI</div>
-					  <div className="hero-18">
-						  2,565,990,000.00{" "}
-						  <span style={{ color: `rgba(${colorStyleHero.r}, ${colorStyleHero.g}, ${colorStyleHero.b}, ${colorStyleHero.a})` }}>DODI</span>
-					  </div>
-				  </div>
-				  <div className="hero-19">
-					  <div className="hero-20">DODI INU Staked</div>
-					  <div className="hero-21">62%</div>
-				  </div>
-			  </div>
+	  <div className="hero">
+		<div className="hero-bg">
+			<div className="hero-2">
+				<div className="hero-3">
+					<div className="column-hero">
+						<img
+							loading="lazy"
+							srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/9bb00a4511cf83fa6cf152d47e994ad38d361bae3c743814d4a8bec0ae1d971d?apiKey=659ef7699bbb4e3e91dbdeecd0d69f59&"
+							className="img-hero"
+						/>
+					</div>
+					<div className="column-hero-2">
+						<div className="hero-4">
+							<div className="hero-5">We all gonna make it</div>
+							<div className="hero-6">
+								Lorem ipsum dolor sit amet consectetur. Nullam in diam cursus
+								purus vitae lectus a. Egestas arcu convallis morbi leo quis
+								velit. Dis faucibus{" "}
+							</div>
+							<div className="hero-7">
+								<div className="hero-8">
+									<div className="column-hero-3">
+										<div className="hero-9">BUY NOW</div>
+									</div>
+									<div className="column-hero-4">
+										<div className="hero-10">STAKE NOW</div>
+									</div>
+								</div>
+							</div>
+							<div className="hero-11">
+								CA: D6NewxS3SPkeEEbvGQax2E4MiCjuvjAhTUkboXZLwKM1
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		  </div>
+			<div className="hero-12">
+				<div className="hero-13">
+					<div className="hero-14">$DODI Price </div>
+					<div className="hero-15">$0.00000412</div>
+				</div>
+				<div className="hero-16">
+					<div className="hero-17">TOTAL DODI</div>
+					<div className="hero-18">
+						2,565,990,000.00{" "}
+						<span style={{ color: `rgba(${colorStyleHero.r}, ${colorStyleHero.g}, ${colorStyleHero.b}, ${colorStyleHero.a})` }}>DODI</span>
+					</div>
+				</div>
+				<div className="hero-19">
+					<div className="hero-20">DODI INU Staked</div>
+					<div className="hero-21">62%</div>
+				</div>
+			</div>
+		</div>
+	  </div>
+	  <div className="about">
+			<div className="about-title">
+				About 
+				<span className="dodi-span">DODI</span>
+			</div>
+			<div className="about-segment">
+				<div className="about-segment-header">MISSION</div>
+				<div className="about-segment-header">PROBLEM</div>
+				<div className="about-segment-header">SOLUTION</div>
+				<div className="about-segment-header">GOAL</div>
+			</div>
+			<Columns space={38} className="about-image-container">
+				<Column width={50}>
+				<Image
+					image="https://cdn.builder.io/api/v1/image/assets/TEMP/b0da2857ff7fbd57107ca800acfb3c83c022d8bdecd00501014df6120215eb57?placeholderIfAbsent=true"
+					backgroundSize="auto"
+					aspectRatio={0.61}
+					noWebp={false}
+					className="about-image"
+				/>
+				</Column>
+				<Column width={50}>
+				<div className="about-content">
+					<Columns space={10} className="about-content-column">
+						<Column width={30.132450331125828}>
+							<Image
+							image="https://cdn.builder.io/api/v1/image/assets/TEMP/501355013552184d924d09465ac1b8adf31d4fe5d71da25cb1772deb6f6d5e05?placeholderIfAbsent=true"
+							backgroundSize="auto"
+							aspectRatio={1}
+							noWebp={false}
+							className="about-image"
+							/>
+						</Column>
+						<Column width={69.86754966887418}>
+							<div className="about-content-column">
+							<div className="about-content-title">About Us</div>
+							<div className="about-content-text">
+								Lorem ipsum dolor sit amet consectetur. Egestas tempus vitae
+								lorem urna luctus amet leo at. Commodo mattis nisl.
+							</div>
+							</div>
+						</Column>
+					</Columns>
+					<Columns space={10} className="about-content-column">
+						<Column width={30.132450331125828}>
+							<Image
+							image="https://cdn.builder.io/api/v1/image/assets/TEMP/501355013552184d924d09465ac1b8adf31d4fe5d71da25cb1772deb6f6d5e05?placeholderIfAbsent=true"
+							backgroundSize="auto"
+							aspectRatio={1}
+							noWebp={false}
+							className="about-image"
+							/>
+						</Column>
+						<Column width={69.86754966887418}>
+							<div className="about-content-column">
+							<div className="about-content-title">About Us</div>
+							<div className="about-content-text">
+								Lorem ipsum dolor sit amet consectetur. Egestas tempus vitae
+								lorem urna luctus amet leo at. Commodo mattis nisl.
+							</div>
+							</div>
+						</Column>
+					</Columns>
+					<Columns space={10} className="about-content-column">
+						<Column width={30.132450331125828}>
+							<Image
+							image="https://cdn.builder.io/api/v1/image/assets/TEMP/501355013552184d924d09465ac1b8adf31d4fe5d71da25cb1772deb6f6d5e05?placeholderIfAbsent=true"
+							backgroundSize="auto"
+							aspectRatio={1}
+							noWebp={false}
+							className="about-image"
+							/>
+						</Column>
+						<Column width={69.86754966887418}>
+							<div className="about-content-column">
+							<div className="about-content-title">About Us</div>
+							<div className="about-content-text">
+								Lorem ipsum dolor sit amet consectetur. Egestas tempus vitae
+								lorem urna luctus amet leo at. Commodo mattis nisl.
+							</div>
+							</div>
+						</Column>
+					</Columns>
+				</div>
+				</Column>
+			</Columns>
+			<div className="about-buy-button">
+				BUY DODI
+			</div>
+      </div>
 	</>
   )
 }
