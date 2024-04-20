100491a3f16952927a74784d038acb673ad354e4
ManLikeTeetos <tolu.mastermindz@gmail.com>
2024-04-21 00:34:32 +0100

    Staking columns

--- src/pages/sections/StakingBody.js
+++ src/pages/sections/StakingBody.js
@@ -2,6 +2,7 @@ import "../../styles/StakingBody.css"
 import Trophy from "../../styles/assets/images/trophy.png"
 import Twitter from "../../styles/assets/images/twitter_stake.png"
 import Telegram from "../../styles/assets/images/telegram_stake.png"
+import Wallet from "../../styles/assets/images/wallet.png"
 
 
 export default function StakingBody(){
@@ -19,7 +20,6 @@ export default function StakingBody(){
 					</div>
 				</div>
 				<div className="stkwrapper">
-
 					<div className="stkcore">
 						<div className="stk-tot-lock">
 							<div className="stk-tot-frame">
@@ -48,10 +48,205 @@ export default function StakingBody(){
 						</div>
 						<div className="stkconnect">
 							<div className="stkconnect-col">
+								<div className="stake-header">
+									<img
+										src={Wallet}
+										alt="Wallet"
+										className="wallet"
+									/>
+									<div className="stake-title">
+										Start Staking
+									</div>
+								</div>
+								<div className="stake-inner">
+									<div className="stake-bio">
+										<span className="stake-bio-text">
+											Stake any amount of DODI get daily staking rewards.
+										</span>
+									</div>
+									<div className="stake-card">
+										<div className="stake-inner-card">
+											<div className="stake-balance">
+												<div className="stake-bal-field">
+													<div className="stake-bal-label">
+														Balance
+													</div>
+													<span className="stake-bal-amt">
+														--
+													</span>
+												</div>
+											</div>
+											<div className="stake-tax">
+												<div className="stake-amount-field">
+													<div className="stake-bal-label">
+														Staking Amount
+													</div>
+													<span className="stake-bal-amt">
+														--
+													</span>
+												</div>
+												<div className="stake-tax-field">
+													<div className="stake-bal-label">
+														Tax
+													</div>
+													<span className="stake-bal-amt">
+														3%
+													</span>
+												</div>
+											</div>
+											<div className="stake-date">
+												<div className="stake-days">
+													<span className="stake-days-txt">
+														10 Days
+													</span>
+												</div>
+												<div className="stake-days">
+													<span className="stake-days-txt">
+														50 Days
+													</span>
+												</div>
+											</div>
+										</div>
+										<div className="stake-btn">
+											<span className="stake-btn-txt">
+												STAKE
+											</span>
+										</div>
+									</div>
+								</div>
 							</div>
 							<div className="stkconnect-col">
+								<div className="stake-header">
+									<img
+										src={Wallet}
+										alt="Wallet"
+										className="wallet"
+									/>
+									<div className="stake-title">
+										Staking Account
+									</div>
+								</div>
+								<div className="stake-inner">
+									<div className="stake-bio">
+										<span className="stake-bio-text">
+											See how much is locked and claim when done staking.
+										</span>
+									</div>
+									<div className="stake-card">
+										<div className="stake-inner-card">
+											<div className="stake-balance">
+												<div className="stake-bal-field">
+													<div className="stake-bal-label">
+														Locked Staking
+													</div>
+													<span className="stake-bal-amt">
+														--
+													</span>
+												</div>
+											</div>
+											<div className="stake-time-div">
+												<div className="stake-time">
+													<span className="timer">
+														Timer
+													</span>
+												</div>
+												<div className="stake-count">
+													<div className="stake-count-in">
+														<span className="stake-count-txt">
+															--
+														</span>
+														<span className="stake-count-title">
+															Days
+														</span>
+													</div>
+													<div className="stake-count-divider">
+														:
+													</div>
+													<div className="stake-count-in">
+														<span className="stake-count-txt">
+															--
+														</span>
+														<span className="stake-count-title">
+															Hours
+														</span>
+													</div>
+													<div className="stake-count-divider">
+														:
+													</div>
+													<div className="stake-count-in">
+														<span className="stake-count-txt">
+															--
+														</span>
+														<span className="stake-count-title">
+															Minutes
+														</span>
+													</div>
+													<div className="stake-count-divider">
+														:
+													</div>
+													<div className="stake-count-in">
+														<span className="stake-count-txt">
+															--
+														</span>
+														<span className="stake-count-title">
+															Seconds
+														</span>
+													</div>
+												</div>
+											</div>
+											<div className="stake-red-balance">
+												<div className="stake-bal-field">
+													<div className="stake-bal-label">
+														Redeemable Balance
+													</div>
+													<span className="stake-bal-amt">
+														--
+													</span>
+												</div>
+											</div>
+										</div>
+										<div className="stake-acc-btn-div">
+											<div className="stake-acc-btn">
+												<span className="stake-btn-txt">
+													RESTAKE
+												</span>
+											</div>
+											<div className="stake-acc-btn">
+												<span className="stake-btn-txt">
+													CLAIM
+												</span>
+											</div>
+										</div>
+									</div>
+								</div>
 							</div>
 							<div className="stkconnect-col">
+								<div className="stake-header">
+									<img
+										src={Wallet}
+										alt="Wallet"
+										className="wallet"
+									/>
+									<div className="stake-title">
+										Reward
+									</div>
+								</div>
+								<div className="stake-reward">
+									<div className="stake-reward-title">
+										<span className="stake-reward-txt">
+											Here are the records of <br />
+											your rewards
+										</span>
+									</div>
+									<div className="stake-records">
+										Records
+									</div>
+									<div className="stake-records-div">
+										<span className="stake-record-txt">
+											No Record
+										</span>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
