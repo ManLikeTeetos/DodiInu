ae0f075c1dd9af8a3a2094510f6bd3de363c256a
emmer12 <laffbaze@gmail.com>
2024-05-15 12:49:17 +0100

    parse unit

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -1,6 +1,6 @@
 import { utils } from "ethers";
 
-export const toBigNumber = (value) => utils.parseEther(value.toString());
-export const fromBigNumber = (value, decimal = 18) =>
-  utils.formatUnits(value.toString(), decimal);
+export const toBigNumber = (value) => utils.parseUnits(value.toString(), 10);
+export const fromBigNumber = (value, decimal = 10) =>
+  utils.formatUnits(value.toString(), (decimal = 10));
 export const currentSeconds = new Date().getTime() / 1000;
